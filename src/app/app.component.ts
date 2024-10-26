8509895017256462eab6a4f1a1c5d7868442b198
KleberMera <klebermera2016@gmail.com>
2024-10-25 22:15:35 -0500

    firts commit cnel pages energy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { NgxSonnerToaster } from 'ngx-sonner';
+import { NavComponent } from "./components/nav/nav.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgxSonnerToaster, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'cnelep-cortes';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
