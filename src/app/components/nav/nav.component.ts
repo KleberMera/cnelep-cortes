a5e32c84b4bd9431f6a54f22d2325b333080bc44
KleberMera <klebermera2016@gmail.com>
2024-10-25 22:36:57 -0500

    navbar interfaces

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 
+interface NavItem {
+  name: string;
+  url: string;
+}
+
+const navItems: NavItem[] = [
+
+  { name: 'Services', url: '#' },
+  { name: 'Pricing', url: '#' },
+  { name: 'Contact', url: '#' },
+];
+
 @Component({
   selector: 'app-nav',
   standalone: true,
@@ -8,5 +20,6 @@ import { Component } from '@angular/core';
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
+  navItems = navItems;
 
 }
